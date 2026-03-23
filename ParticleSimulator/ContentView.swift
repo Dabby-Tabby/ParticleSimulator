2797b18c66b1f6deefb4a2281c9c9ba28365b215
Nicholas Watts <thekilowat@gmail.com>
2026-03-23 17:03:46 -0500

    Initial Commit

---
+++ ParticleSimulator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ParticleSimulator
+//
+//  Created by Nick Watts on 3/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ParticleSimulator/ParticleSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParticleSimulatorApp.swift
+//  ParticleSimulator
+//
+//  Created by Nick Watts on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct ParticleSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
